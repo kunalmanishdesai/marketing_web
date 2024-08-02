b97ca3b28e757b3519c59729f3a3394c50c14b13
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-08-02 17:01:29 +0530

    made how component responsive

--- src/components/How.jsx
+++ src/components/How.jsx
@@ -1,5 +1,5 @@
 import { Analytics, PhoneIphone, DeviceHub } from "@mui/icons-material";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 
 const cardContents = [
@@ -36,118 +36,109 @@ const cardContents = [
 ];
 
 const How = () => {
-    const firstRowCards = cardContents.slice(0, 3);
-    const secondRowCards = cardContents.slice(3);
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
     const renderCards = (cards) => (
-        <Box
-            sx={{
-                width: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-evenly',
-                flexWrap: 'wrap',
-                py: 5,
-                px: 5,
-            }}
-        >
+        <Grid container spacing={4} justifyContent="center">
             {cards.map((card, index) => (
-                <Box
-                    key={index}
-                    sx={{
-                        width: 300,
-                        height: 200,
-                        perspective: 1000, // Perspective for the 3D effect
-                        m: 4, // Margin for spacing between cards
-                    }}
-                >
+                <Grid item xs={12} sm={6} md={4} key={index}>
                     <Box
                         sx={{
+                            perspective: 1000, // Perspective for the 3D effect
+                            height: 300,
                             width: '100%',
-                            height: '100%',
-                            position: 'relative', // Ensure positioning is relative to the container
-                            transformStyle: 'preserve-3d',
-                            transition: 'transform 0.6s',
-                            transformOrigin: 'center',
-                            '&:hover': {
-                                transform: 'rotateY(180deg)', // Rotate the inner box on hover
-                            },
                         }}
                     >
-                        {/* Front Side */}
                         <Box
                             sx={{
-                                position: 'absolute',
                                 width: '100%',
                                 height: '100%',
-                                backfaceVisibility: 'hidden',
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                p: 4,
-                                backgroundColor: 'white',
-                                borderRadius: 3,
-                                boxShadow: 5,
+                                position: 'relative', // Ensure positioning is relative to the container
+                                transformStyle: 'preserve-3d',
+                                transition: 'transform 0.6s',
+                                transformOrigin: 'center',
+                                '&:hover': {
+                                    transform: 'rotateY(180deg)', // Rotate the inner box on hover
+                                },
                             }}
                         >
-                            {card.icon}
-                            <Typography
-                                variant="h6"
-                                component="div"
+                            {/* Front Side */}
+                            <Box
                                 sx={{
-                                    fontWeight: 'bold',
+                                    position: 'absolute',
+                                    width: '100%',
+                                    height: '100%',
+                                    backfaceVisibility: 'hidden',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    p: 4,
+                                    backgroundColor: 'white',
+                                    borderRadius: 3,
+                                    boxShadow: 5,
                                 }}
                             >
-                                {card.title}
-                            </Typography>
-                        </Box>
+                                {card.icon}
+                                <Typography
+                                    variant="h6"
+                                    component="div"
+                                    sx={{
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    {card.title}
+                                </Typography>
+                            </Box>
 
-                        {/* Back Side */}
-                        <Box
-                            sx={{
-                                position: 'absolute',
-                                width: '100%',
-                                height: '100%',
-                                backfaceVisibility: 'hidden',
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                p: 4,
-                                backgroundColor: 'white',
-                                borderRadius: 3,
-                                boxShadow: 5,
-                                transform: 'rotateY(180deg)',
-                            }}
-                        >
-                            <Typography variant="body2" component="div" sx={{ mt: 1 }}>
-                                {card.description}
-                            </Typography>
+                            {/* Back Side */}
+                            <Box
+                                sx={{
+                                    position: 'absolute',
+                                    width: '100%',
+                                    height: '100%',
+                                    backfaceVisibility: 'hidden',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    p: 4,
+                                    backgroundColor: 'white',
+                                    borderRadius: 3,
+                                    boxShadow: 5,
+                                    transform: 'rotateY(180deg)',
+                                }}
+                            >
+                                <Typography variant="body2" component="div" sx={{ mt: 1 }}>
+                                    {card.description}
+                                </Typography>
+                            </Box>
                         </Box>
                     </Box>
-                </Box>
+                </Grid>
             ))}
-        </Box>
+        </Grid>
     );
 
     return (
         <Box
             sx={{
                 width: '100%',    // Full width of the parent
-                height: '100vh',  // Full viewport height
                 display: 'flex',
                 flexDirection: 'column',
                 overflow: 'hidden', // Prevents overflow of content
                 justifyContent: 'start', // Align content at the top
                 alignItems: 'center', // Center content horizontally
-                py: 3
+                py: 3,
+                px: isSmallScreen ? 2 : 5, // Adjust padding based on screen size
             }}
         >
             <Box
                 sx={{
                     textAlign: 'left', // Align text to the left
                     borderRadius: 2, // Rounded corners
+                    px: 2, // Padding for small screens
                 }}
             >
                 <Typography
@@ -157,14 +148,14 @@ const How = () => {
                         fontWeight: 700, // Bolder font weight
                         color: 'text.primary',
                         lineHeight: 1.2, // Tighter line height for a compact look
+                        mb: 4, // Margin bottom for spacing
                     }}
                 >
                     How we deliver Amazing Results
                 </Typography>
             </Box>
 
-            {renderCards(firstRowCards)}
-            {renderCards(secondRowCards)}
+            {renderCards(cardContents)}
 
         </Box>
     );
