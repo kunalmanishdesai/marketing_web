7d29bd01156df23e8866abe5d0231a2f8f60e098
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-08-02 18:59:05 +0530

    refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,8 @@
 import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import { Home } from "@mui/icons-material";
 import React from "react";
-import { blue } from "@mui/material/colors";
 
 const Header = () => {
-
-    const navItemStyle = {
-        mx: 1.5, // Margin between items
-        fontWeight: '500', // Semi-bold
-        textTransform: 'uppercase', // Uppercase text
-        color: 'text.secondary', // Subtle color
-        transition: 'color 0.3s ease', // Smooth color transition
-        '&:hover': {
-            color: blue[500], // Change color on hover
-            cursor: 'pointer' // Pointer cursor on hover
-        }
-    };
 
     return (
         <Box sx={{ flexGrow: 1,maxHeight: '64px',}}>
